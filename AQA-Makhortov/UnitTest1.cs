ada8ec44751f40b54f78e06889ec41fff7814c46
SunnyMKV <tabbb1234@gmail.com>
2026-08-18 16:42:13 +0200

    HW 13

--- AQA-Makhortov/UnitTest1.cs
+++ AQA-Makhortov/UnitTest1.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Net.Http.Json;
+using System.Text.Json;
 
 namespace AQA_Makhortov;
 
@@ -11,18 +12,19 @@ public class Tests
     {
         _client = new HttpClient()
         {
-            BaseAddress = new Uri("http://reqres.in/api/")
+            BaseAddress = new Uri("https://reqres.in/api/")
         };
         _client.DefaultRequestHeaders.Add("x-api-key", "free_user_3I3a9hG7CShczDI7fbelCNX7ZtF");
     }
 
     [Test]
-    /*public async Task Test1()
+    public async Task Test1()
     {
         using HttpResponseMessage response = await _client.GetAsync("users/2");
         response.EnsureSuccessStatusCode();
-    }*/
-
+    }
+    
+    [Test]
     public async Task Test2()
     {
         using HttpResponseMessage response = await _client.GetAsync("users/2");
@@ -30,7 +32,39 @@ public class Tests
         UserResponseDto userResponse = JsonSerializer.Deserialize<UserResponseDto>(jsonGet);
         UserDataDto user = userResponse.Data;
     }
+    
+    [Test]
+    public async Task Test3()
+    {
+        CreateUserRequestDTO newUserFields = new CreateUserRequestDTO
+        {
+            Name = "Larry",
+            Job = "Oracle"
+        };
+        using HttpResponseMessage response = await _client.PostAsJsonAsync("users", newUserFields);
+        string jsonPost = await response.Content.ReadAsStringAsync();
+        CreateUserResponseDTO newUserFieldsResponse = JsonSerializer.Deserialize<CreateUserResponseDTO>(jsonPost);
+    }
+    
+    [Test]
+    public async Task Test4()
+    {
+        CreateUserRequestDTO newUserFields = new CreateUserRequestDTO
+        {
+            Name = "Larry",
+            Job = "Valve"
+        };
+        using HttpResponseMessage response = await _client.PutAsJsonAsync("users/2", newUserFields);
+        response.EnsureSuccessStatusCode();
+    }
 
+    [Test]
+    public async Task Test5()
+    {
+        using HttpResponseMessage response = await _client.DeleteAsync("users/2");
+        response.EnsureSuccessStatusCode();
+    }
+    
     [OneTimeTearDown]
     public void TearDown()
     {
